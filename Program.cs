b18baf4e65a1069d4353a9b28fc8481f9b922b1a
MotasemAlsuradi <alsuradym@gmail.com>
2024-11-13 23:25:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace First_OOP_Project
+{
+    public interface IEngine
+    {
+        void Start();
+    }
+
+    public class Engine : IEngine
+    {
+        public void Start()
+        {
+            Console.WriteLine("Engine started.");
+        }
+    }
+
+    public class Car
+    {
+        private readonly IEngine _engine;
+
+        public Car(IEngine engine)
+        {
+            _engine = engine;
+        }
+
+        public void Start()
+        {
+            _engine.Start();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEngine engine = new Engine();
+            Car car = new Car(engine);
+
+            car.Start();
+
+        }
+    }
+}
